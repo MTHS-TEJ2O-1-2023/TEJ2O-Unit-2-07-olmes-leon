457aad5254828c60774628ce2ac2a9ddc422e88b
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-10-14 16:37:43 -0400

    fix

--- main.ts
+++ main.ts
@@ -5,4 +5,30 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    cookies += clickValue
+    basic.showNumber(cookies)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(cookies)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+})
+let cookies = 0
+let clickValue = 0
+clickValue = 1
+basic.showNumber(cookies)
